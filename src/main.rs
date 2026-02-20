a0e5131782c18e2c1a13fcb781ebc0b805a18074
Senski <m.senski@gmail.com>
2026-02-20 16:08:02 +0100

    send basic packt for testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use r_dns::dns::{DNSEncodable, DNSHeader, DNSPacket, DNSQuestion, DnsName};
+use std::net::UdpSocket;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // create the Query packet
+    let id = rand::random::<u16>();
+    let query = DNSPacket {
+        header: DNSHeader::new_query(id),
+        question: DNSQuestion {
+            name: DnsName("google.com".to_string()),
+            type_: 1, // Type A
+            class: 1, // Class IN
+        },
+    };
+
+    // "0.0.0.0:0" means:
+    // 0.0.0.0 -> Listen on all my local network interfaces (WiFi, Ethernet).
+    // :0      -> Let the OS pick any available random port for me.
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
+
+    let mut query_buf: Vec<u8> = Vec::new();
+    query.write_bytes(&mut query_buf)?;
+    socket
+        .send_to(&query_buf, "8.8.8.8:53")
+        .expect("Failed to sent DNS packet");
+
+    let mut response_buf: Vec<u8> = Vec::new();
+
+    match socket.recv(&mut response_buf) {
+        Ok(_) => println!("{response_buf:x?}"),
+        Err(err) => eprintln!("Encountered error while trying to receive response: {err}"),
+    };
+    Ok(())
 }
